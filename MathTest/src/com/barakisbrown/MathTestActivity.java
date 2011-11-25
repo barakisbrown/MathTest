cd887323e9fd3781addc51e13c1d524ecb042744
Matthew Brown <lokislayer@gmail.com>
2011-11-24 19:16:29 -0600

    First Commit to begin MathTest Project

---
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -0,0 +1,13 @@
+package com.barakisbrown;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MathTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
