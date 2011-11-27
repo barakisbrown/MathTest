10fbdb868a02de2f9750411cb5d63b2b3d738c7f
Matthew Brown <barakisbrown.com>
2011-11-26 21:42:50 -0600

    Activity should be ready as a base for this project ongoing

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -2,12 +2,25 @@ package com.barakisbrown;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MathTestActivity extends Activity {
+public class MathTestActivity extends Activity implements OnClickListener 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button beginButton = (Button)findViewById(R.id.beginTestButton);
+        beginButton.setOnClickListener(this);
     }
+
+	public void onClick(View v) 
+	{
+		Toast.makeText(this,"Not Impelemented Yet",Toast.LENGTH_LONG).show();
+	}
 }
