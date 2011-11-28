20c347ee1bdb70b3f3229f8c015530929b5c147b
mattlabtop <mattlabtop@mattlabtop-PC>
2011-11-28 11:21:00 -0600

    Dynamic Number Image Class

---
+++ MathTest/src/com/barakisbrown/TestProblemActivity.java
@@ -0,0 +1,13 @@
+package com.barakisbrown;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestProblemActivity extends Activity 
+{
+	public void onCreate(Bundle savedState)
+	{
+		super.onCreate(savedState);
+		setContentView(R.layout.tstproblemactivity);
+	}
+}
