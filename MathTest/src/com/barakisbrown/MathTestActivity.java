c2124272d51813410ce120173153d044ab6eb808
mattlabtop <mattlabtop@mattlabtop-PC.local.tld>
2011-11-29 11:29:48 -0600

    Code to begin Quiz

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -3,6 +3,7 @@ package com.barakisbrown;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 
 public class MathTestActivity extends Activity implements OnClickListener 
 {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
@@ -22,11 +23,22 @@ public class MathTestActivity extends Activity implements OnClickListener
 
 	public void onClick(View v) 
 	{
-		Toast.makeText(this,"Not Impelemented Yet",Toast.LENGTH_LONG).show();
-		// this is where I need to call LayoutTestingActivity
-		// Begin Test
-		Intent intent = new Intent(this,TestProblemActivity.class);
-		startActivity(intent);
-		// End Test
+		Quiz quiz = Quiz.initQuiz();
+		// This is where I will add the code to begin launching the quiz by calling ProblemActivity
+		// number of times equal to the size of the quiz
+		final int MAXQuizSize = quiz.getNumProblems();
+		// Load Quiz data
+		quiz.LoadQuiz();
+		// Begin Quiz Loop
+		
+		for (int currentProblemNumber = 1;currentProblemNumber < MAXQuizSize;currentProblemNumber++)
+		{
+			Bundle dataStore = new Bundle();
+			dataStore.putInt("currentProblemNumber", currentProblemNumber);
+			dataStore.putInt("MaxNumQuestions",MAXQuizSize);
+			dataStore.putParcelable("QUIZ",(Parcelable) quiz);
+			// Start Again After This
+		}
+		
 	}
 }
