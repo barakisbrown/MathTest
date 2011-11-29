817d697b39bf9d157565a11f9c6e8c6d0fe73a30
mattlabtop <mattlabtop@mattlabtop-PC.local.tld>
2011-11-29 11:30:45 -0600

    Actual workhorse of the application

---
+++ MathTest/src/com/barakisbrown/ProblemActivity.java
@@ -0,0 +1,16 @@
+package com.barakisbrown;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ProblemActivity extends Activity implements OnClickListener 
+{
+
+	public void onClick(View arg0) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
