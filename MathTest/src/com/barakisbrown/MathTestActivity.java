047cc20e750a41ad42a2d27c39d0e189ceb31699
Matthew Brown <barakisbrown.com>
2011-11-30 12:41:43 -0600

    Updated MathTestActivity

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -22,25 +22,40 @@ public class MathTestActivity extends Activity implements OnClickListener
 
 	public void onClick(View v) 
 	{
+		int currentProblemNumber = 0;
 		// This is where I will add the code to begin launching the quiz by calling ProblemActivity
 		// number of times equal to the size of the quiz
 		final int MAXQuizSize = quiz.getNumProblems();
 		// Load Quiz data
 		quiz.LoadQuiz();
 		// Begin Quiz Loop
+		Bundle data = new Bundle();
+		Intent intent = new Intent(this,ProblemActivity.class);
+		data.putInt("ProblemNUmber",currentProblemNumber);
+		data.putInt("MaxNumProblems",MAXQuizSize);
+		data.putInt("LeftSide", quiz.getFirst(currentProblemNumber));
+		data.putInt("RightSide",quiz.getSecond(currentProblemNumber));
+		intent.putExtras(data);
+		startActivityForResult(intent,Activity.RESULT_OK);
 		
 		
-		for (int currentProblemNumber = 1;currentProblemNumber < MAXQuizSize;currentProblemNumber++)
-		{
-			Bundle dataStore = new Bundle();
-			dataStore.putInt("ProblemNumber", currentProblemNumber);
-			dataStore.putInt("MaxNumProblems",MAXQuizSize);
-			// Start Again After This
-			Intent intent = new Intent(this,ProblemActivity.class);
-			startActivity(intent);	
-		}
-		// now we need to display the score page
-		Intent scoreIntent = new Intent(this,ScoreDisplayActivity.class);
-		startActivity(scoreIntent);
 	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		 super.onActivityResult(requestCode, resultCode, data);
+	     switch(requestCode)
+	     {
+	     case Activity.RESULT_OK:
+	        	 if (resultCode == RESULT_OK)
+	        	 {
+	        		 Bundle res = data.getExtras();
+	        		 int numProblem = res.getInt("NumProblem");
+	        		 int userGuess = res.getInt("UserGuessed");
+	        		 quiz.setGuess(userGuess,numProblem);
+	        	 }
+	        	break;
+	     } // end switch
+	}
+	
 }
