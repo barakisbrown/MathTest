44d4a8addb5951872e789fe4b55f74eafea048e1
Matthew Brown <barakisbrown.com>
2011-11-29 18:57:33 -0600

    Add Loop to show the different Quizs

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -3,14 +3,13 @@ package com.barakisbrown;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MathTestActivity extends Activity implements OnClickListener 
 {
+	public static Quiz quiz = Quiz.initQuiz();
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -23,7 +22,6 @@ public class MathTestActivity extends Activity implements OnClickListener
 
 	public void onClick(View v) 
 	{
-		Quiz quiz = Quiz.initQuiz();
 		// This is where I will add the code to begin launching the quiz by calling ProblemActivity
 		// number of times equal to the size of the quiz
 		final int MAXQuizSize = quiz.getNumProblems();
@@ -31,14 +29,18 @@ public class MathTestActivity extends Activity implements OnClickListener
 		quiz.LoadQuiz();
 		// Begin Quiz Loop
 		
+		
 		for (int currentProblemNumber = 1;currentProblemNumber < MAXQuizSize;currentProblemNumber++)
 		{
 			Bundle dataStore = new Bundle();
-			dataStore.putInt("currentProblemNumber", currentProblemNumber);
-			dataStore.putInt("MaxNumQuestions",MAXQuizSize);
-			dataStore.putParcelable("QUIZ",(Parcelable) quiz);
+			dataStore.putInt("ProblemNumber", currentProblemNumber);
+			dataStore.putInt("MaxNumProblems",MAXQuizSize);
 			// Start Again After This
+			Intent intent = new Intent(this,ProblemActivity.class);
+			startActivity(intent);	
 		}
-		
+		// now we need to display the score page
+		Intent scoreIntent = new Intent(this,ScoreDisplayActivity.class);
+		startActivity(scoreIntent);
 	}
 }
