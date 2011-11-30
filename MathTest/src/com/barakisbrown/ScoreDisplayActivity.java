4732ce9e4c46c45a059e77d236b48c6403df2490
Matthew Brown <barakisbrown.com>
2011-11-29 18:56:12 -0600

    Shows the total score for the Quiz

---
+++ MathTest/src/com/barakisbrown/ScoreDisplayActivity.java
@@ -0,0 +1,29 @@
+package com.barakisbrown;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.barakisbrown.Quiz;
+
+public class ScoreDisplayActivity extends Activity 
+{
+	private Quiz quiz = MathTestActivity.quiz;
+	private int totalScore = 0;
+	private int totalCorrect = 0;
+	private int numProblems = 0;
+	public void OnCreate(Bundle savedState)
+	{
+		super.onCreate(savedState);
+		setContentView(R.layout.scoredisplay);
+		
+		final TextView totalScoreDisplay = (TextView)findViewById(R.id.finalScoreLabel);
+		
+		String totalDispStr = totalScoreDisplay.getText().toString();
+		
+		totalScore = quiz.determineScore();
+		totalCorrect = quiz.getNumCorrect();
+		numProblems = quiz.getNumProblems();
+		totalDispStr = String.format(totalDispStr,numProblems,totalCorrect,totalScore);
+		totalScoreDisplay.setText(totalDispStr);
+	}
+}
