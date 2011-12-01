329046cc18b2045f63c0da03c697007df1a4b198
Matthew Brown <barakisbrown.com>
2011-11-30 23:10:35 -0600

    Updated Activity

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -3,13 +3,18 @@ package com.barakisbrown;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MathTestActivity extends Activity implements OnClickListener 
 {
-	public static Quiz quiz = Quiz.initQuiz();
+	private int PROBLEMCREATED = 0;
+	private int currentProblemNumber = 0;
+	private int LeftSide = 0;
+	private int RightSide = 0;
+	private Quiz quiz;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -18,44 +23,51 @@ public class MathTestActivity extends Activity implements OnClickListener
         setContentView(R.layout.main);
         final Button beginButton = (Button)findViewById(R.id.beginTestButton);
         beginButton.setOnClickListener(this);
+        quiz = Quiz.initQuiz();
     }
 
 	public void onClick(View v) 
 	{
-		int currentProblemNumber = 0;
 		// This is where I will add the code to begin launching the quiz by calling ProblemActivity
 		// number of times equal to the size of the quiz
 		final int MAXQuizSize = quiz.getNumProblems();
 		// Load Quiz data
 		quiz.LoadQuiz();
 		// Begin Quiz Loop
+		
+		LeftSide = quiz.getFirst(currentProblemNumber);
+		RightSide = quiz.getSecond(currentProblemNumber);
+
 		Bundle data = new Bundle();
 		Intent intent = new Intent(this,ProblemActivity.class);
-		data.putInt("ProblemNUmber",currentProblemNumber);
-		data.putInt("MaxNumProblems",MAXQuizSize);
-		data.putInt("LeftSide", quiz.getFirst(currentProblemNumber));
-		data.putInt("RightSide",quiz.getSecond(currentProblemNumber));
+		data.putInt("ProblemNumber",currentProblemNumber++);
+		data.putInt("MaxNumProblem",MAXQuizSize);
+		data.putInt("LeftSide", LeftSide);
+		data.putInt("RightSide",RightSide);
 		intent.putExtras(data);
-		startActivityForResult(intent,Activity.RESULT_OK);
-		
-		
+		this.startActivityForResult(intent,PROBLEMCREATED);
+		Log.d("MathTestActivity OnClick()","Were here");
 	}
 	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,Intent data)
 	{
-		 super.onActivityResult(requestCode, resultCode, data);
-	     switch(requestCode)
-	     {
-	     case Activity.RESULT_OK:
-	        	 if (resultCode == RESULT_OK)
+		Log.d("MathTestActivity", "Begin onActivityResult");
+		super.onActivityResult(requestCode, resultCode, data);
+		switch(requestCode)
+	    {
+	     case 0:
+	        	 if (resultCode == Activity.RESULT_OK)
 	        	 {
 	        		 Bundle res = data.getExtras();
-	        		 int numProblem = res.getInt("NumProblem");
+	        		 int numProblem = res.getInt("ProblemNumber");
 	        		 int userGuess = res.getInt("UserGuessed");
-	        		 quiz.setGuess(userGuess,numProblem);
+	        		 Log.d("Problem Number =",String.valueOf(numProblem));
+	        		 Log.d("User Guessed   =",String.valueOf(userGuess));
+	        		 quiz.setGuess(userGuess, currentProblemNumber);
 	        	 }
 	        	break;
-	     } // end switch
-	}
-	
+	    } // end switch
+		Log.d("MathTestActivity", "End onActivityResult");
+	}	
 }
