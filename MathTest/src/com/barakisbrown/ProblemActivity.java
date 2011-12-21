554004970bfbe98a84660af035add10bfc86e037
Matthew Brown <barakisbrown.com>
2011-12-20 23:16:55 -0600

    This now is whole app. I have tried to create a loop inside the OnClick and when it is done it will call ScoreDisplayActivity to show the results.

--- MathTest/src/com/barakisbrown/ProblemActivity.java
+++ MathTest/src/com/barakisbrown/ProblemActivity.java
@@ -12,85 +12,122 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.barakisbrown.Quiz;
 
 public class ProblemActivity extends Activity implements OnClickListener 
 {
 	private int numProblem = 0;
-	private int maxProblem = 0;
+	private int maxProblem = 5;
 	private int UserGuessed = 0;
+	private int UserAnswered = 0;
 	private int LeftSide;
 	private int RightSide;
+	private int ActualAnswer;
+	private String problemLabelString;
+	private String displayString;
+	private Quiz quiz = Quiz.initQuiz();
+	private List<Integer> display;
+	// Controls Here
+	TextView problem;
+	Button submit;
+	ImageView firstLeft;
+	ImageView secondLeft;
+	ImageView firstRight;
+	ImageView secondRight;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.problem_layout);
-		
-		Bundle data = getIntent().getExtras();
-		
-		numProblem = data.getInt("ProblemNumber");
-		maxProblem = data.getInt("MaxNumProblem");
-		LeftSide = data.getInt("LeftSide");
-		RightSide = data.getInt("RightSide");
-		
-		final TextView problem = (TextView)findViewById(R.id.problemLabel);
-		String problemStr = problem.getText().toString();
-		
-		if (numProblem == 0)
-			problemStr = String.format(problemStr,1,maxProblem);
-		else
-			problemStr = String.format(problemStr, numProblem,maxProblem);
-		problem.setText(problemStr);
-		
-		final Button submit = (Button)findViewById(R.id.SubmitProblem);
+		// initialize my own varaiables here
+		quiz.LoadQuiz();
+		problemLabelString = getResources().getString(R.string.ProblemLabel);
+		// initialize form controls
+		problem = (TextView)findViewById(R.id.problemLabel);
+		firstLeft = (ImageView)findViewById(R.id.leftNum1);
+		secondLeft = (ImageView)findViewById(R.id.leftNum2);
+		firstRight = (ImageView)findViewById(R.id.rightNum1);
+		secondRight = (ImageView)findViewById(R.id.rightNum2);
+		submit = (Button)findViewById(R.id.SubmitProblem);
 		submit.setOnClickListener(this);
-		
+		// now lets create the first problem dynamically
 		displayLayout();
+	}
+	
+	public void onClick(View v) 
+	{
+		EditText result = (EditText)findViewById(R.id.guessTxtBox);
+		UserGuessed = Integer.parseInt(result.getText().toString());
+		quiz.setGuess(UserGuessed, numProblem);
+		
+		if (numProblem < 4)
+		{
+			numProblem++;
+			result.setText("");
+			displayLayout();
+		}
+		else
+		{
+			Intent scoreIT = new Intent(ProblemActivity.this,ScoreDisplayActivity.class);
+			
+			quiz.determineScore();
+			scoreIT.putExtra("Score",quiz.getScore());
+			scoreIT.putExtra("Correct",quiz.getNumCorrect());
+			scoreIT.putExtra("NumProblems",maxProblem);
+			
+			startActivity(scoreIT);
+		}
+		
+		
+		
+		
 	}
 
 	private void displayLayout()
 	{
+		LeftSide = quiz.getFirst(numProblem);
+		RightSide = quiz.getSecond(numProblem);
+		ActualAnswer = LeftSide + RightSide;
+		// update problem label
+		displayString = String.format(problemLabelString, numProblem + 1,maxProblem);
+		problem.setText(displayString);
+		// display numbers
 	    displayLeftSide();
 	    displayRightSide();
 	}
 
 	  private void displayLeftSide()
 	  {
-	    List<Integer> display = getDisplay(LeftSide);
+	    display = getDisplay(LeftSide);
 	    if (display.size() == 1)
 	    {
-	      int resID = ((Integer)display.get(0)).intValue();
-	      ImageView firstNumber = (ImageView)findViewById(R.id.leftNum1);
-	      firstNumber.setImageResource(resID);
+	      int resID = display.remove(0);
+	      firstLeft.setImageResource(resID);
 	    }
 	    else
 	    {
-	      int resID_1 = ((Integer)display.get(0)).intValue();
-	      int resID_2 = ((Integer)display.get(1)).intValue();
-	      ImageView firstNumber = (ImageView)findViewById(R.id.leftNum1);
-	      ImageView secondNumber = (ImageView)findViewById(R.id.leftNum2);
-	      firstNumber.setImageResource(resID_1);
-	      secondNumber.setImageResource(resID_2);
+	      int resID_1 = display.remove(0);
+	      int resID_2 = display.remove(0);
+	      firstLeft.setImageResource(resID_1);
+	      secondLeft.setImageResource(resID_2);
 	    }
 	  }
 
 	  private void displayRightSide()
 	  {
-	    List<Integer> display = getDisplay(RightSide);
+	    display = getDisplay(RightSide);
 	    if (display.size() == 1)
 	    {
-	      int resID = ((Integer)display.get(0)).intValue();
-	      ImageView firstNumber = (ImageView)findViewById(R.id.rightNum1);
-	      firstNumber.setImageResource(resID);
+	      int resID = display.remove(0);
+	      firstRight.setImageResource(resID);
 	    }
 	    else
 	    {
-	      int resID_1 = ((Integer)display.get(0)).intValue();
-	      int resID_2 = ((Integer)display.get(1)).intValue();
-	      ImageView firstNumber = (ImageView)findViewById(R.id.rightNum1);
-	      ImageView secondNumber = (ImageView)findViewById(R.id.rightNum2);
-	      firstNumber.setImageResource(resID_1);
-	      secondNumber.setImageResource(resID_2);
+	      int resID_1 = display.remove(0);
+	      int resID_2 = display.remove(0);
+	      firstRight.setImageResource(resID_1);
+	      secondRight.setImageResource(resID_2);
 	    }
 	  }
 
@@ -142,23 +179,5 @@ public class ProblemActivity extends Activity implements OnClickListener
 	    }
 
 	    return retList;
-	}
-	  
-	public void onClick(View v) 
-	{
-		EditText result = (EditText)findViewById(R.id.guessTxtBox);
-		UserGuessed = Integer.parseInt(result.getText().toString());
-		returnData();
-	}
-
-	private void returnData()
-	{
-		Bundle data = new Bundle();
-		data.putInt("ProblemNumber",numProblem);
-		data.putInt("UserGuessed",UserGuessed);
-		Intent retIntent = new Intent();
-		retIntent.putExtras(data);
-		setResult(Activity.RESULT_OK,retIntent);
-		finish();
 	}
 }
