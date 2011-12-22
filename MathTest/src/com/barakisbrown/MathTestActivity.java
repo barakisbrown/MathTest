309ddaf7af2c98d87ff12982246e9cf480cdb035
Matthew Brown <barakisbrown.com>
2011-12-21 22:22:13 -0600

    Add where ProblemActivity will return with a result which then ScoreDisplayActivity will be called to show the score so that if the back button is pressed it will be call back to the MathTestActivity

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MathTestActivity extends Activity implements OnClickListener 
 {
+	private static final int SCORE_IT = 1000;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) 
@@ -22,6 +23,37 @@ public class MathTestActivity extends Activity implements OnClickListener
 	public void onClick(View v) 
 	{
 		Intent IT = new Intent(this,ProblemActivity.class);
-		startActivity(IT);
+		this.startActivityForResult(IT,SCORE_IT);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		switch(requestCode)
+		{
+		case SCORE_IT:
+		{
+			if (resultCode == Activity.RESULT_OK)
+			{
+				
+				double score = data.getDoubleExtra("Score", 0.0);
+				int correct = data.getIntExtra("Correct",0);
+				int numProblem = data.getIntExtra("NumProblems",0);
+				// now lets call the scoredisplayactivity
+				Intent scoreIT = new Intent(this,ScoreDisplayActivity.class);
+	
+				scoreIT.putExtra("Score",score);
+				scoreIT.putExtra("Correct",correct);
+				scoreIT.putExtra("NumProblems",numProblem);
+				
+				startActivity(scoreIT);
+			}
+		}
+		default:
+			break;
+		}
 	}	
+	
+	
 }
