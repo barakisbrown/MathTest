fbf9660240d14f3ba24f7e4834b0199ef895b698
barakis <barakis@barakis-PC>
2013-03-17 18:53:40 -0500

    Post Merge Modifications

--- MathTest/src/com/barakisbrown/MathTestActivity.java
+++ MathTest/src/com/barakisbrown/MathTestActivity.java
@@ -3,71 +3,6 @@ package com.barakisbrown;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
-/***
- * This is the master MathTestActivity Class.
- * @author Matt
- *
- */
-public class MathTestActivity extends Activity implements OnClickListener
-{
-	private static final int SCORE_IT = 1000;
-	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        final Button begin = (Button)findViewById(R.id.StartQuiz);
-        begin.setOnClickListener(this);
-     
-    }
-    
-   	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
-	{
-		super.onActivityResult(requestCode, resultCode, data);
-		switch(requestCode)
-		{
-		case SCORE_IT:
-		{
-			if (resultCode == Activity.RESULT_OK)
-			{
-				
-				double score = data.getDoubleExtra("Score", 0.0);
-				int correct = data.getIntExtra("Correct",0);
-				int numProblem = data.getIntExtra("NumProblems",0);
-				// now lets call the Activity ScoreDisplayActivity
-				Intent scoreIT = new Intent(this,ScoreDisplayActivity.class);
-	
-				scoreIT.putExtra("Score",score);
-				scoreIT.putExtra("Correct",correct);
-				scoreIT.putExtra("NumProblems",numProblem);
-				
-				startActivity(scoreIT);
-			}
-		}
-		default:
-			break;
-		}
-	}
-
-
-
-	public void onClick(View arg0) 
-	{
-		Intent IT = new Intent(this,ProblemActivity.class);
-		this.startActivityForResult(IT,SCORE_IT);
-		
-	}	
-	
-	
-=======
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -168,6 +103,4 @@ public class MathTestActivity extends Activity implements OnItemSelectedListener
 
 
    	public void onNothingSelected(AdapterView<?> arg0){ }
-
->>>>>>> refs/remotes/origin/spinner_test
 }
