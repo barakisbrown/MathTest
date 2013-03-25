56ea678b020b4696861bc03d82f3f69445128e8c
Matthew Brown <barakis@barakisbrown.com>
2013-03-25 14:24:08 -0500

    Oops..I forgot the constructor initialization so I had to take care of it.

--- MathTest/src/com/barakisbrown/ProblemDisplay.java
+++ MathTest/src/com/barakisbrown/ProblemDisplay.java
@@ -1,5 +1,6 @@
 package com.barakisbrown;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,6 +12,8 @@ public class ProblemDisplay
 	
 	public ProblemDisplay()
 	{
+		display = new ArrayList<Integer>();
+		leftSide = rightSide = 0;
 		
 	}
 	
