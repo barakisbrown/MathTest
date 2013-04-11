d5d920bff19ef34e641568c389593a851fc67c7f
barakis <barakis@barakisbrown.com>
2013-04-11 11:09:18 -0500

    New class to see if I can add images dynamically which then I take this example and then expand it to my other parts of my program

---
+++ MathTest/src/com/barakisbrown/DynamicImageTest.java
@@ -0,0 +1,27 @@
+package com.barakisbrown;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import com.barakisbrown.ProblemHelper;
+
+public class DynamicImageTest extends Activity implements OnClickListener
+{
+	private int leftSide = 100;
+	private int rightSide = 100;
+	private int total = 200;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+	}
+
+	public void onClick(View v) 
+	{
+		
+	}
+	
+}
