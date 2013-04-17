0924723a17c466d962e95aa465dc008d16950648
barakis <barakis@barakis-PC>
2013-04-16 22:12:55 -0500

    Clone of ProblemActivity but I am using dynamic displayed images instead

---
+++ MathTest/src/com/barakisbrown/ProblemActivity2.java
@@ -0,0 +1,21 @@
+package com.barakisbrown;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class ProblemActivity2 extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
