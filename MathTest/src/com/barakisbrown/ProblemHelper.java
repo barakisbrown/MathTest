373b1c83b8630409424d40f4bfc3d33183bb2316
barakis <barakis@barakisbrown.com>
2013-04-26 12:26:42 -0500

    Modified it so that if I pass number 10, it will not parse it instead it will use the new image I just created.

--- MathTest/src/com/barakisbrown/ProblemHelper.java
+++ MathTest/src/com/barakisbrown/ProblemHelper.java
@@ -80,52 +80,59 @@ public class ProblemHelper
 	}
 	
 	private void setDisplay(int Number)
-	{
-	  
-	    String numStr = String.valueOf(Number);
-	    Log.d("ProblemHelper setDisplay","Number = " + Number);
-	    Log.d("ProblemHelper setDisplay","numStr = " + numStr);
-
-	    int Length = numStr.length();
-
-	    for (int counter = 0; counter < Length; counter++)
+	{    
+	    // add a bit of logic for the case where just the number '10' shows up. Instead of breaking it into two,
+	    // I have created a new image for the number 10.
+	    if (Number == 10)
 	    {
-	      int displayNumber = Character.getNumericValue(numStr.charAt(counter));
-	      switch (displayNumber)
-	      {
-	      case 0:
-	    	  display.add(R.drawable.num0);
-	    	  break;
-	      case 1:
-	    	  display.add(R.drawable.num1);
-	    	  break;
-	      case 2:
-	    	  display.add(R.drawable.num2);
-	    	  break;
-	      case 3:
-	    	  display.add(R.drawable.num3);
-	    	  break;
-	      case 4:
-	    	  display.add(R.drawable.num4);
-	    	  break;
-	      case 5:
-	    	  display.add(R.drawable.num5);
-	    	  break;
-	      case 6:
-	      display.add(R.drawable.num6);
-	    	  break;
-	      case 7:
-	    	  display.add(R.drawable.num7);
-	    	  break;
-	      case 8:
-	    	  display.add(R.drawable.num8);
-	    	  break;
-	      case 9:
-	    	  display.add(R.drawable.num9);
-	    	  break;
-	      default:
-	    	  break;
-	      }
+	    	display.add(R.drawable.num10);
 	    }
+	    else
+	    {
+	    	String numStr = String.valueOf(Number);
+		    Log.d("ProblemHelper setDisplay","Number = " + Number);
+		    Log.d("ProblemHelper setDisplay","numStr = " + numStr);  
+	    	
+		    int Length = numStr.length();
+		    for (int counter = 0; counter < Length; counter++)
+		    {
+		      int displayNumber = Character.getNumericValue(numStr.charAt(counter));
+		      switch (displayNumber)
+		      {
+		      case 0:
+		    	  display.add(R.drawable.num0);
+		    	  break;
+		      case 1:
+		    	  display.add(R.drawable.num1);
+		    	  break;
+		      case 2:
+		    	  display.add(R.drawable.num2);
+		    	  break;
+		      case 3:
+		    	  display.add(R.drawable.num3);
+		    	  break;
+		      case 4:
+		    	  display.add(R.drawable.num4);
+		    	  break;
+		      case 5:
+		    	  display.add(R.drawable.num5);
+		    	  break;
+		      case 6:
+		      display.add(R.drawable.num6);
+		    	  break;
+		      case 7:
+		    	  display.add(R.drawable.num7);
+		    	  break;
+		      case 8:
+		    	  display.add(R.drawable.num8);
+		    	  break;
+		      case 9:
+		    	  display.add(R.drawable.num9);
+		    	  break;
+		      default:
+		    	  break;
+		      }
+		    }
+		}
 	}
 }
