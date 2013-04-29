9a3b9b0fd9bc6053cb45941317ab37915b91f5d2
barakis <barakis@barakisbrown.com>
2013-04-29 17:30:12 -0500

    Added Code so that it can use my linear layout that I have added

--- MathTest/src/com/barakisbrown/ScoreDisplayActivity2.java
+++ MathTest/src/com/barakisbrown/ScoreDisplayActivity2.java
@@ -5,7 +5,10 @@ import java.util.Iterator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.barakisbrown.ProblemHelper;
@@ -32,12 +35,13 @@ public class ScoreDisplayActivity2 extends Activity
 		
 		final ImageView probTotal = (ImageView)findViewById(R.id.probTotal);
 		final ImageView probCorrect = (ImageView)findViewById(R.id.probCorrect);
+		final LinearLayout probTotalScore = (LinearLayout)findViewById(R.id.probTotalScore);
 		helper = new ProblemHelper();
 		// get values from the intent that called this activity
 		totalScore = getIntent().getDoubleExtra("Score",100.0); 
 		totalCorrect = getIntent().getIntExtra("Correct",5);
 		numProblems = getIntent().getIntExtra("NumProblems",5);
-		// manipulate totalscore where it will show 100% not .100%
+		// manipulate total score where it will show 100% not .100%
 		totalScore = totalScore * 100.00;
 		// NEW SECTION
 		// For the following section I am going to be needing an iterator for displaying images to the screen
@@ -51,6 +55,27 @@ public class ScoreDisplayActivity2 extends Activity
 		probCorrect.setImageResource(itor.next());
 		itor.remove();
 		// I need to display the user TotalScore to the Screen Here
+		// Since this will be a variable number from 2 to 4 digits .. ie 2% to 100% .. I am using a view
+		// then dynamically adding the numbers followed by the percent sign to it.
+		ImageView iv = new ImageView(this);
+		int dblvalue = (int)totalScore;
+		itor = helper.builder(dblvalue);
+		
+		while(itor.hasNext())
+		{
+			iv.setScaleType(ScaleType.FIT_CENTER);
+			iv.setImageResource(itor.next());
+			// Might have to add a layout here
+			probTotalScore.addView(iv);
+			itor.remove();
+		}
+		// add percent sign
+		iv.setScaleType(ScaleType.FIT_CENTER);
+		iv.setImageResource(R.drawable.percent);
+		probTotalScore.addView(iv);
+		//
+		// Code below will be the display of the problems missed.
+		
 		
 		
 		
